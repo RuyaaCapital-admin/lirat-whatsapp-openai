d6b07f74bd2aa01f9311a0d06c1a5ee6a0aae30e
RuyaaCapital-admin <admin@ruyaacapital.com>
2025-10-23 18:40:27 +0400

    Fix Next.js config warning - remove deprecated appDir option

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   // Ensure API routes work properly
   async rewrites() {
     return [];
